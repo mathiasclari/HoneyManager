4e16a45f4d995e9e18a7c520c792ce30c7262ce9
Mat <66589714+PxLib@users.noreply.github.com>
2022-05-17 13:44:50 +0200

    Fixed PlayTime Added config.yml

--- src/main/java/fun/mcbee/bungee/honeynetwork/HoneyNetwork.java
+++ src/main/java/fun/mcbee/bungee/honeynetwork/HoneyNetwork.java
@@ -2,6 +2,7 @@ package fun.mcbee.bungee.honeynetwork;
 
 import com.google.common.io.ByteStreams;
 import fun.mcbee.bungee.honeynetwork.AdminCommands.CheckOnlineCommand;
+import fun.mcbee.bungee.honeynetwork.AdminCommands.PlayTime;
 import fun.mcbee.bungee.honeynetwork.FriendsCommands.FriendCommand;
 import fun.mcbee.bungee.honeynetwork.FriendsCommands.FriendsCommand;
 import fun.mcbee.bungee.honeynetwork.ManagerCommands.MaintenanceCommand;
@@ -13,8 +14,11 @@ import fun.mcbee.bungee.honeynetwork.Util.EventListener;
 import fun.mcbee.bungee.honeynetwork.data.FData;
 import fun.mcbee.bungee.honeynetwork.data.FInfo;
 import fun.mcbee.bungee.honeynetwork.data.MYSQL;
+import fun.mcbee.bungee.honeynetwork.data.PData;
 import net.md_5.bungee.BungeeCord;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -30,8 +34,11 @@ public final class HoneyNetwork extends Plugin {
 
     public static boolean maintenance = false;
 
-    public static Map<UUID, FInfo> listFriendsInfos = new HashMap<UUID, FInfo>(); //uuid, info
-    public static Map<UUID, FData> listFriendData = new HashMap<UUID, FData>(); //server, info
+    public static Map<UUID, FInfo> listFriendsInfos = new HashMap<>();
+
+    public static Map<UUID, FData> listFriendData = new HashMap<>();
+
+    public static Map<UUID, PData> listPlayerData = new HashMap<>();
 
     public Configuration config;
 
@@ -41,34 +48,31 @@ public final class HoneyNetwork extends Plugin {
         return plugin;
     }
 
-    @Override
     public void onEnable() {
         plugin = this;
-        ProxyServer.getInstance().getPluginManager().registerListener(this, new BungeeSettings());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, (Listener)new BungeeSettings());
         ProxyServer.getInstance().getPluginManager().registerListener(this, new EventListener(this));
-        ProxyServer.getInstance().getPluginManager().registerListener(this, new StaffChatEvent());
-//Commands
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new HubCommand());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new CheckOnlineCommand());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new MaintenanceCommand());
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new BuildServer());
-        BungeeCord.getInstance().getPluginManager().registerCommand(this, new FriendCommand("f"));
-        BungeeCord.getInstance().getPluginManager().registerCommand(this, new FriendCommand("friend"));
-        BungeeCord.getInstance().getPluginManager().registerCommand(this, new FriendsCommand());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, (Listener)new StaffChatEvent());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, (Command)new HubCommand());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, (Command)new CheckOnlineCommand());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, (Command)new MaintenanceCommand());
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, (Command)new BuildServer());
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, (Command)new FriendCommand("f"));
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, (Command)new FriendCommand("friend"));
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, (Command)new FriendsCommand());
+        BungeeCord.getInstance().getPluginManager().registerCommand(this, (Command)new PlayTime());
         LoadData();
-        //maintenance = config.getBoolean("maintenance");
     }
 
     private void LoadData() {
         GetDefaultConfig();
-        String host = config.getString("mysql.host");
-        String database = config.getString("mysql.databse");
-        String username = config.getString("mysql.username");
-        String password = config.getString("mysql.password");
-        int port = config.getInt("mysql.port");
+        String host = this.config.getString("mysql.host");
+        String database = this.config.getString("mysql.databse");
+        String username = this.config.getString("mysql.username");
+        String password = this.config.getString("mysql.password");
+        int port = this.config.getInt("mysql.port");
         MYSQL.Login(host, database, username, password, port);
         MYSQL.CreateTables();
-
     }
 
     void GetDefaultConfig() {
@@ -109,49 +113,12 @@ public final class HoneyNetwork extends Plugin {
         }
     }
 
-    /* public void saveDefaultConfig() {
-         File file = new File(getDataFolder(), "config.yml");
-         try {
-             if (file.exists()) {
-                 InputStream is = getResourceAsStream("config.yml");
-                 Throwable localThrowable6 = null;
-
-                 try {
-                     OutputStream os = new FileOutputStream(file);
-                     try {
-                         ByteStreams.copy(is, os);
-                     } catch (Throwable localThrowable1) {
-                         throw localThrowable1;
-                     }
-                 } catch (Throwable localThrowable4) {
-                     localThrowable6 = localThrowable4;
-                     throw localThrowable4;
-                 } finally {
-                     if (is != null)
-                         if (localThrowable6 != null) {
-                             try {
-                                 is.close();
-                             } catch (Throwable localThrowable5) {
-                                 localThrowable6.addSuppressed(localThrowable5);
-                             }
-                         } else {
-                             is.close();
-                         }
-                 }
-             }
-             this.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-     }
- */
     public static void AddFriendConnection(UUID uuid, String name, boolean update) {
         FData fd = listFriendData.get(uuid);
-        if(fd != null) {
+        if (fd != null) {
             fd.AddConnectionCount();
-            if(update) {
+            if (update)
                 fd.UpdateName(name);
-            }
         } else {
             fd = new FData(uuid, name);
             listFriendData.put(uuid, fd);
@@ -159,15 +126,11 @@ public final class HoneyNetwork extends Plugin {
     }
 
     public static void RemoveFriendConnections(List<UUID> uuids) {
-        for(UUID uuid : uuids) {
+        for (UUID uuid : uuids)
             RemoveFriendConnections(uuid);
-        }
     }
 
     public static void RemoveFriendConnections(UUID uuid) {
-        listFriendData.get(uuid).RemoveConnectionCount();
+        ((FData)listFriendData.get(uuid)).RemoveConnectionCount();
     }
-
-
 }
-
--- src/main/java/fun/mcbee/bungee/honeynetwork/data/PData.java
+++ src/main/java/fun/mcbee/bungee/honeynetwork/data/PData.java
@@ -1,55 +1,59 @@
 package fun.mcbee.bungee.honeynetwork.data;
 
-import fun.mcbee.bungee.honeynetwork.HoneyNetwork;
-import net.md_5.bungee.BungeeCord;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
-public class PData {
+import fun.mcbee.bungee.honeynetwork.HoneyNetwork;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.plugin.Plugin;
 
+public class PData {
     private UUID uuid;
+
     private long[] days = new long[7];
+
     private Long totalTime;
+
     private Long joinTime;
+
     private Date lastJoinBefore;
 
-    public PData(UUID uuid) {
+    private int dayDiff;
+
+    public PData(final UUID uuid) {
         this.uuid = uuid;
-        this.joinTime = System.currentTimeMillis();
-        BungeeCord.getInstance().getScheduler().runAsync(HoneyNetwork.getInstance(), new Runnable() {
-            @Override
+        this.joinTime = Long.valueOf(System.currentTimeMillis());
+        BungeeCord.getInstance().getScheduler().runAsync((Plugin) HoneyNetwork.getInstance(), new Runnable() {
             public void run() {
                 Statement stmt = null;
-                String query = "SELECT * FROM networkmanagerdatabase where UUID = '" + uuid + "';";
+                String query = "SELECT *, DATEDIFF(now(), LastLogin) as diff FROM networkmanagerdatabase where UUID = '" + uuid + "';";
                 try {
                     stmt = MYSQL.GetConnection().createStatement();
                     ResultSet rs = stmt.executeQuery(query);
-
                     while (rs.next()) {
-                        totalTime = rs.getLong("TotalTime");
-                        for(int i = 0; i < days.length; i++) {
-                            days[i] = rs.getLong("Day" + (i + 1));
-                        }
+                        PData.this.totalTime = Long.valueOf(rs.getLong("TotalTime"));
+                        for (int i = 0; i < PData.this.days.length; i++)
+                            PData.this.days[i] = rs.getLong("Day" + (i + 1));
+                        PData.this.lastJoinBefore = rs.getDate("LastLogin");
+                        PData.this.dayDiff = rs.getInt("diff");
                     }
                 } catch (SQLException e) {
                     e.printStackTrace();
                 } finally {
-                    if(stmt != null) {
+                    if (stmt != null)
                         try {
                             stmt.close();
                         } catch (SQLException e) {
                             e.printStackTrace();
                         }
-                    }
                 }
-
-                if(totalTime == null) {//first join
-                    totalTime = 0l;
-
+                if (PData.this.totalTime == null) {
+                    PData.this.totalTime = Long.valueOf(0L);
                     stmt = null;
                     query = "INSERT INTO networkmanagerdatabase (UUID) VALUES ('" + uuid + "');";
                     try {
@@ -58,17 +62,14 @@ public class PData {
                     } catch (SQLException e) {
                         e.printStackTrace();
                     } finally {
-                        if(stmt != null) {
+                        if (stmt != null)
                             try {
                                 stmt.close();
                             } catch (SQLException e) {
                                 e.printStackTrace();
                             }
-                        }
                     }
-
-                } else {
-
+                } else if (PData.this.dayDiff == 0) {
                     stmt = null;
                     query = "UPDATE networkmanagerdatabase SET LastLogin = now() WHERE (`UUID` = '" + uuid + "');";
                     try {
@@ -77,48 +78,104 @@ public class PData {
                     } catch (SQLException e) {
                         e.printStackTrace();
                     } finally {
-                        if(stmt != null) {
+                        if (stmt != null)
                             try {
                                 stmt.close();
                             } catch (SQLException e) {
                                 e.printStackTrace();
                             }
+                    }
+                } else {
+                    for (int i = 7; i > PData.this.dayDiff; i--) {
+                        int index = i - PData.this.dayDiff;
+                        if (index < 0) {
+                            PData.this.days[i] = PData.this.days[index];
+                        } else {
+                            PData.this.days[i] = 0L;
                         }
                     }
-
+                    stmt = null;
+                    query = "UPDATE networkmanagerdatabase SET LastLogin = now(), Day1 = " + PData.this.days[0] + ", Day2 = " + PData.this.days[1] + ", Day3 = " + PData.this.days[2] + ", Day4 = " + PData.this.days[3] + ", Day5 = " + PData.this.days[4] + ", Day6 = " + PData.this.days[5] + ", Day7 = " + PData.this.days[6] + " WHERE (`UUID` = '" + uuid + "');";
+                    try {
+                        stmt = MYSQL.GetConnection().createStatement();
+                        stmt.executeUpdate(query);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (stmt != null)
+                            try {
+                                stmt.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                    }
                 }
-
-
             }
         });
-
     }
 
     public void Logout() {
-        long timeDiffInSec = (System.currentTimeMillis() - this.joinTime) / 1000 + totalTime;
-
+        long timeDiffInSec = (System.currentTimeMillis() - this.joinTime.longValue()) / 1000L + this.totalTime.longValue();
         Statement stmt = null;
-        String query = "UPDATE networkmanagerdatabase SET TotalTime = " + timeDiffInSec + " WHERE (UUID = '" + uuid + "');";
+        String query = "UPDATE networkmanagerdatabase SET TotalTime = " + timeDiffInSec + ", Day1 = " + timeDiffInSec + " WHERE (UUID = '" + this.uuid + "');";
         try {
             stmt = MYSQL.GetConnection().createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                totalTime = rs.getLong("TotalTime");
-                for(int i = 0; i < days.length; i++) {
-                    days[i] = rs.getLong("Day" + (i + 1));
-                }
-            }
+            stmt.executeUpdate(query);
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if(stmt != null) {
+            if (stmt != null)
                 try {
                     stmt.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
+        }
+    }
+
+    public long GetSecondsTotalOnline() {
+        return this.totalTime.longValue() + (System.currentTimeMillis() - this.joinTime.longValue()) / 1000L;
+    }
+
+    public long GetSecondsWeekOnline() {
+        long time = (System.currentTimeMillis() - this.joinTime.longValue()) / 1000L;
+        for (int i = 0; i < 7; i++)
+            time += this.days[i];
+        return time;
+    }
+
+    public static String GetTextTime(Long t) {
+        List<Integer> times = new ArrayList<>();
+        int years = (int)(t.longValue() / 3.1556926E7D);
+        times.add(Integer.valueOf(years));
+        t = Long.valueOf(t.longValue() - (long)(years * 3.1556926E7D));
+        int days = (int)(t.longValue() / 86400.0D);
+        times.add(Integer.valueOf(days));
+        t = Long.valueOf(t.longValue() - (long)(days * 86400.0D));
+        int hours = (int)(t.longValue() / 3600.0D);
+        times.add(Integer.valueOf(hours));
+        t = Long.valueOf(t.longValue() - (long)(hours * 3600.0D));
+        int minutes = (int)(t.longValue() / 60.0D);
+        times.add(Integer.valueOf(minutes));
+        int seconds = t.intValue() - (int)(minutes * 60.0D);
+        times.add(Integer.valueOf(seconds));
+        String timeDispaly = "";
+        String[] suffS = { "year", "day", "hour", "minute", "second" };
+        for (int i = 0; i < times.size(); i++) {
+            int time = ((Integer)times.get(i)).intValue();
+            if (time != 0) {
+                String suffix = suffS[i];
+                if (!timeDispaly.isEmpty())
+                    timeDispaly = timeDispaly + ", ";
+                if (time == 1) {
+                    timeDispaly = timeDispaly + time + " " + suffix;
+                } else if (time > 1) {
+                    timeDispaly = timeDispaly + time + " " + suffix + "s";
+                }
             }
         }
+        if (timeDispaly.isEmpty())
+            timeDispaly = "0 seconds";
+        return timeDispaly;
     }
 }
